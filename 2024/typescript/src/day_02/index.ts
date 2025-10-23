b4450f840fcc78398e45382c84eae73d0b20483a
uros <u.ignjatovic00@gmail.com>
2025-10-23 16:38:14 +0200

    2024 day 2 p2

--- 2024/typescript/src/day_02/index.ts
+++ 2024/typescript/src/day_02/index.ts
@@ -1,6 +1,33 @@
 import { readFileSync } from "node:fs";
 import { importPathFuckery, printOutput } from "../utils.js";
 
+enum Order {
+  ASC,
+  DESC,
+}
+
+function isSafe(arr: number[]): boolean {
+  let order: Order | null = null;
+  let last = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    const diff = arr[i] - last;
+
+    if (diff === 0) return false;
+
+    if (i === 1) {
+      order = diff > 0 ? Order.ASC : Order.DESC;
+    }
+
+    if (order === Order.ASC && (diff > 3 || diff < 1)) return false;
+    if (order === Order.DESC && (diff < -3 || diff > -1)) return false;
+
+    last = arr[i];
+  }
+
+  return true;
+}
+
 function main(test: boolean) {
   const file = readFileSync(
     importPathFuckery(`src/day_02/${test ? "test" : "input"}`),
@@ -10,21 +37,34 @@ function main(test: boolean) {
     .split(/\r?\n/);
 
   let p1 = 0;
+  let p2 = 0;
 
   for (const line of file) {
     const arr = line.split(" ").map(Number);
-    const diffs = arr.slice(1).map((n, i) => n - arr[i]);
-    const order = diffs[0] > 0 ? "asc" : "desc";
-    const safe = diffs.every(
-      (d) =>
-        (order === "asc" && d >= 1 && d <= 3) ||
-        (order === "desc" && d <= -1 && d >= -3),
-    );
 
-    if (safe) p1++;
+    const safe = isSafe(arr);
+    if (safe) {
+      p1++;
+      p2++;
+      continue;
+    }
+
+    let dampened = false;
+    for (let i = 0; i < arr.length; i++) {
+      const modified = arr.slice(0, i).concat(arr.slice(i + 1));
+
+      if (isSafe(modified)) {
+        dampened = true;
+        break;
+      }
+    }
+
+    if (dampened) p2++;
   }
 
-  printOutput("02", p1, 0);
+  printOutput("02", p1, p2);
 }
 
 export default main;
+
+// xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
