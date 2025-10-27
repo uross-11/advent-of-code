78ef8baa43ba86cd1d942e626156f61bee213c92
uros <u.ignjatovic00@gmail.com>
2025-10-27 23:05:30 +0100

    2024 day 5 p2

--- 2024/typescript/src/day_05/index.ts
+++ 2024/typescript/src/day_05/index.ts
@@ -6,14 +6,18 @@ function main(f: string): Solution {
 
   const instructions: number[][] = [];
   const lines: number[][] = [];
-
   const incorrectLines: number[][] = [];
+  const instructionMap = new Map<number, Set<number>>();
 
   for (const line of f.split(/\r?\n/)) {
-    if (line.trim() === "") continue;
-
+    if (!line.trim()) continue;
     if (line.includes("|")) instructions.push(line.split("|").map(Number));
     else lines.push(line.split(",").map(Number));
+  }
+
+  for (const [a, b] of instructions) {
+    if (!instructionMap.has(a)) instructionMap.set(a, new Set());
+    instructionMap.get(a)?.add(b);
   }
 
   for (const instruction of instructions) {
@@ -22,26 +26,24 @@ function main(f: string): Solution {
       const right = lines[i].indexOf(instruction[1]);
 
       if (left !== -1 && right !== -1 && left > right) {
-        // sort
-        // extract
-        // push
-
-        const a = instruction[0];
-        const b = instruction[1];
-
-        lines[i][left] = b;
-        lines[i][right] = a;
-
-        const removed = lines.splice(i, 1).flat();
-
-        incorrectLines.push(removed);
+        incorrectLines.push(lines.splice(i, 1).flat());
       }
     }
   }
 
-  console.log("Incorrect lines:", incorrectLines);
+  const sortedIncorrect = incorrectLines.map((line) =>
+    line.sort((a, b) => {
+      if (instructionMap.get(a)?.has(b)) return -1;
+      if (instructionMap.get(b)?.has(a)) return 1;
+      return 0;
+    }),
+  );
 
   p1 = lines.map((l) => l[Math.floor(l.length / 2)]).reduce((a, c) => a + c);
+
+  p2 = sortedIncorrect
+    .map((l) => l[Math.floor(l.length / 2)])
+    .reduce((a, c) => a + c);
 
   return { p1, p2 };
 }
